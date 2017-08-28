f8da3f5531387f0f89bb1d922bdec4b6566243b2
ctrhodes <ctrhodes@users.noreply.github.com>
2017-08-27 20:47:43 -0500

    Add files via upload

---
+++ sra_fastqDump.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+pathname=( "/home/chris/geoSra/BV27me3H4K20me3/raw/SRP074862" "/home/chris/geoSra/BV27me3H4K20me3/raw/SRX603399" )
+
+for d in "${pathname[@]}"
+do
+
+cd $d
+
+pwd
+
+
+for i in $(ls -d $d/*/)
+do
+cd ${i%%/}
+echo  ${i%%/}
+
+for sample in *sra
+do
+name=$(echo ${sample} | sed 's/.sra//')
+ls
+echo "sample is "$sample
+echo "name is "$name
+
+fastq-dump.2.6.3 $name
+
+echo dump done!
+
+done
+
+
+echo leaving_$(ps -aef | grep -o `pwd`)
+
+cd ../
+
+done
+
+
+done
